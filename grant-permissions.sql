7c32d3f2d52c65e3821113ca8309f67caa6c5c04
tommyfan34 <fanxiao001@126.com>
2022-03-16 11:43:33 -0700

    finish up

--- grant-permissions.sql
+++ grant-permissions.sql
@@ -1,5 +1,5 @@
-CREATE USER 'appadmin'@'localhost' IDENTIFIED BY 'adminpw';
-CREATE USER 'appclient'@'localhost' IDENTIFIED BY 'clientpw';
+CREATE USER 'nbaadmin'@'localhost' IDENTIFIED BY 'adminpw';
+CREATE USER 'nbaclient'@'localhost' IDENTIFIED BY 'clientpw';
 -- Can add more users or refine permissions
 GRANT ALL PRIVILEGES ON shelterdb.* TO 'appadmin'@'localhost';
 GRANT SELECT ON shelterdb.* TO 'appclient'@'localhost';
